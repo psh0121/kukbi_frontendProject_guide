a255781609b5daebc579e3147c4f74c21d3feb29
psh0121 <tngus2057@gmail.com>
2022-12-04 23:23:38 +0900

    [221125] section 레이아웃 설정

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 (() => {
     
-  let yOffset = 0;
+  let yOffset = 0;                // 스크롤 위치값
+  let currentSection = 0;         // 현재 섹션 번호
 
   // section 정보 집합
   const sectionSet = [
@@ -15,7 +16,7 @@
       // section-1의 정보들
       {
           height : 0,
-          hMultiple : 5,
+          hMultiple : 2,
           objs : {
               container : document.querySelector('#section-1')
           }
@@ -32,11 +33,38 @@ const setLayout = function()
   for(let i = 0; i < sectionSet.length; i++)
   {
       sectionSet[i].height = window.innerHeight * sectionSet[i].hMultiple;
-      console.log('클라이언트 창 높이 : ' + window.innerHeight);
-      console.log(i + '번째 섹션 높이 : ' + sectionSet[i].height);
-      sectionSet[i].objs.container.style.height = `${sectionSet[0].height}px`;
+      sectionSet[i].objs.container.style.height = `${sectionSet[i].height}px`;
   }
 
+}
+
+const getCurrentSection = function()
+{
+  let section = 0;
+
+  let segment = [
+      sectionSet[0].height,
+      sectionSet[0].height + sectionSet[1].height
+  ];
+
+  if(yOffset <= segment[0])
+  {
+      section = 0;
+  }
+  else if((yOffset > segment[0]) &&
+          (yOffset <= segment[1]))
+  {
+      section = 1;
+  }
+  else 
+  {
+  }
+  return section;
+}
+
+const setBodyID = function(section)
+{
+  document.body.setAttribute('id', `show-section${section}`);
 }
 
 ////////////////////////////////////////////////////////
@@ -46,17 +74,14 @@ const setLayout = function()
   window.addEventListener('scroll', () => {
       yOffset = window.scrollY;
 
-      console.log(yOffset);
+      // 현재 섹션값을 가지고 온 뒤 body 아이디 설정
+      currentSection = getCurrentSection();       // 스크롤할때마다 섹션위치를 업데이트
+      setBodyID(currentSection);  // currentSection을 통해 받아온 섹션 위치를 통해 body의 id값 새로고침
   });
 
   // load : 모든 작업이 끝났다.
   window.addEventListener('load', () => {
-      // section-0하고 section-1하고의 높이를 설정한다.
+      // 섹션에 대한 높이를 설정한다.
       setLayout();
   })
-
-
-
-
-
 })();
